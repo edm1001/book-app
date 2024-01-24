72d214d1abdc37f71fbaa5aa62ca30040d5beb4a
edm1001 <edmer_franciz@yahoo.com>
2024-01-24 12:34:27 -0600

    create get and update operations

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 import {PORT , mongoDBURL} from "./config.js"
 import mongoose from 'mongoose';
 import {Book} from "./models/bookModel.js"
@@ -35,6 +35,48 @@ app.post('/books', async (req, res) => {
     } catch (err) {
         console.log(err.message);
         res.status(500).send({message: err.message});
+    }
+})
+// routte to get all books from db
+app.get('/books', async (req, res) => {
+    try{
+        const books = await Book.find({});
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }catch (err) {
+        console.log(err.message);
+        res.status(500).send({message: err.message});
+    }
+})
+// routte to get 1 books from db
+app.get('/books/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+        const book = await Book.findById(id);
+        return res.status(200).json({book});
+    }catch (err) {
+        console.log(err.message);
+        res.status(500).send({message: err.message});
+    }
+})
+
+// update a book
+app.put('/books/:id', async (req, res) => {
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({message: 'Please fill in all required fields'});
+        }
+        const {id} = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({message: err.message})
     }
 })
 
